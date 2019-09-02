2fe41ed752143c575c26842e5276799d0c6425eb
roastwind <muzi131313@gmail.com>
2019-09-02 18:05:51 +0800

    commonPlugins by webpack3.x

--- examples/webpack/commonPlugin/webpack.config.js
+++ examples/webpack/commonPlugin/webpack.config.js
@@ -1,6 +1,33 @@
+const path = require('path');
+const webpack = require('webpack')
+const resolve = _path => path.join(__dirname, _path)
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-  entry: 'src/index.js',
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js'
+  },
   output: {
-    filename: 'bundle.js'
-  }
+    filename: '[name].js',
+    path: resolve('dist')
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function(module){
+        return module.context && module.context.includes('node_modules');
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'split chunks'
+    })
+  ]
 }
