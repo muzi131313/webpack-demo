866ab7698d176e99900a11ea3cd1443eb07af794
roastwind <muzi131313@gmail.com>
2019-11-04 10:33:37 +0800

    mpa: init

---
+++ examples/webpack/mpa/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" class="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+.logo
+    @include px2rem('width', 75px)
+    @include px2rem('height', 75px)
+</style>
